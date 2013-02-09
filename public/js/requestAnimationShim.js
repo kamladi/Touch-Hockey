3aaa685704316c0a06395f65a3be854a99101af4
Kedar Amladi <kamladi@gmail.com>
2013-02-08 23:59:51 -0500

    added previously ignored external js shims/libraries

---
+++ public/js/requestAnimationShim.js
@@ -0,0 +1,11 @@
+//requestAnimationFrame shim w/setTimeoutFallback
+window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     || 
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+})();
